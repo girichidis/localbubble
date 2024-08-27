42e10a48a73301c5b03a0bfa1ef8b506ccfd17ef
Philipp Girichidis <philipp@girichidis.com>
2024-08-27 11:04:02 +0200

    more stuff

--- philipp/plot.py
+++ philipp/plot.py
@@ -2,10 +2,21 @@ import pickle
 import numpy as np
 import matplotlib.pyplot as plt
 
-def _read(pickle_path):
+def _read_Ltot(pickle_path):
     with open(pickle_path, 'rb') as f:
         data = pickle.load(f)
     return data["simtime_Myr"], data["total_lumi"]
+
+def _read_Rmed(pickle_path):
+    with open(pickle_path, 'rb') as f:
+        data = pickle.load(f)
+    return data["simtime_Myr"], np.median(data["radius"][data["bubble_open"]==0])
+
+def _read_fopen(pickle_path):
+    with open(pickle_path, 'rb') as f:
+        data = pickle.load(f)
+    return data["simtime_Myr"], np.median(np.sum(data["bubble_open"])/data["bubble_open"].size)
+
 
 cmap = plt.get_cmap('twilight')
 colors = cmap(np.linspace(0.1,0.9,6))
@@ -23,7 +34,9 @@ for i, cd, label in zip(range(6), cd_names, cd_labels):
         pickle_path = "data-float32/SILCC_hdf5_plt_cnt_"+str(file_no).zfill(4)+"-r0150-"+cd+"-data-float32.pkl"
         #pickle_path2 = "data-float32/SILCC_hdf5_plt_cnt_"+str(file_no).zfill(4)+"-r0150-c000050-data-float32.pkl"
 
-        t, L = _read(pickle_path)
+        #t, L = _read_Ltot(pickle_path)
+        #t, L = _read_Rmed(pickle_path)
+        t, L = _read_fopen(pickle_path)
         global_times.append(t)
         global_Xlum.append(L)
 
@@ -31,5 +44,7 @@ for i, cd, label in zip(range(6), cd_names, cd_labels):
 
 ax.legend()
 ax.set_xlabel("time (Myr)")
-ax.set_ylabel("L_X (erg/s)")
-fig.savefig("total-Xlumi-all-files.pdf", bbox_inches="tight")
+#ax.set_ylabel("L_X (erg/s)")
+#ax.set_ylabel("R_cd (pc)")
+ax.set_ylabel("fraction open")
+fig.savefig("fraction-open-all-files.pdf", bbox_inches="tight")
